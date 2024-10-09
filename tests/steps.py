675b950dc56ceb2594549056b35473042df4c976
Steffen Hirte <shirte@users.noreply.github.com>
2024-10-09 10:48:34 +0200

    Adapt unit tests

--- tests/steps.py
+++ tests/steps.py
@@ -1,7 +1,19 @@
 from np_scout import NPScoutModel
-from pytest_bdd import given
+from pytest_bdd import given, parsers, when
 
 
-@given("the NPScout model", target_fixture="model")
+@given("the NPScout model", target_fixture="predictor")
 def np_scout_model():
     return NPScoutModel()
+
+
+@when(
+    parsers.parse("the model generates predictions for the molecule representations"),
+    target_fixture="predictions",
+)
+def predictions(representations, predictor, input_type):
+    return predictor.predict(
+        representations,
+        input_type=input_type,
+        output_format="record_list",
+    )
